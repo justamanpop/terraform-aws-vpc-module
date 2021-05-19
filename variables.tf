ee40c248ea6d73e5be68b66c146bfcd30914f49e
Anish Sekhar <anisekha@publicisgroupe.net>
2021-05-19 09:52:42 +0530

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_id" {
+  description = "VPC ID for vpc in which subnets are to be created"
+}
+
+variable "subnet_name_prefix" {
+  description = "this string will be prepended to all the subnet names"
+}
